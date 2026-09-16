3f6d97967400faee42f4a4a99bc8e748d548df0d
krc456-commits <krc.456@gmail.com>
2026-09-16 13:32:54 -0400

    Add another rights-cleared brewery storefront photo

--- src/data/allBreweries.ts
+++ src/data/allBreweries.ts
@@ -1,5 +1,19 @@
 import { breweries as coreBreweries, areas } from "./breweries";
 import { breweryAdditions } from "./breweryAdditions";
+import type { BreweryImage } from "./breweries";
 
-export const breweries = [...coreBreweries, ...breweryAdditions].sort((a, b) => a.name.localeCompare(b.name));
+const imageOverrides: Record<string, BreweryImage> = {
+  "hitchhiker-sharpsburg": {
+    url: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Hitchhiker_brewpub%2C_Pittsburgh.jpg",
+    alt: "Hitchhiker Brewing taproom in Pittsburgh",
+    sourceLabel: "Wikimedia Commons",
+    sourceUrl: "https://commons.wikimedia.org/wiki/File:Hitchhiker_brewpub,_Pittsburgh.jpg",
+    license: "Creative Commons licensed"
+  }
+};
+
+export const breweries = [...coreBreweries, ...breweryAdditions]
+  .map((brewery) => imageOverrides[brewery.slug] ? { ...brewery, image: imageOverrides[brewery.slug] } : brewery)
+  .sort((a, b) => a.name.localeCompare(b.name));
+
 export { areas };
