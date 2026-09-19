a2dfa367e68f45050233573f66e27c6bc5f609c7
krc456-commits <krc.456@gmail.com>
2026-09-19 18:30:50 -0400

    Map brewery locations to canonical profiles

--- src/data/breweryProfiles.ts
+++ src/data/breweryProfiles.ts
@@ -1,6 +1,21 @@
 export const breweryProfileRoutes: Record<string, string> = {
   "hitchhiker-sharpsburg": "/breweries/hitchhiker",
   "hitchhiker-mt-lebanon": "/breweries/hitchhiker",
+  "dancing-gnome": "/breweries/dancing-gnome",
+  "old-thunder": "/breweries/old-thunder",
+  "east-end": "/breweries/east-end",
+  "penn-brewery": "/breweries/penn-brewery",
+  "church-brew-works": "/breweries/church-brew-works",
+  "grist-house": "/breweries/grist-house",
+  "grist-house-command": "/breweries/grist-house",
+  "grist-house-beer-crib": "/breweries/grist-house",
+  "grist-house-beer-market": "/breweries/grist-house",
+  "brew-gentlemen": "/breweries/brew-gentlemen",
+  "cinderlands-warehouse": "/breweries/cinderlands",
+  "cinderlands-wexford": "/breweries/cinderlands",
+  "allegheny-city-brewing": "/breweries/allegheny-city",
+  "allegheny-city-brighton-heights": "/breweries/allegheny-city",
+  "abjuration-hazelwood": "/breweries/abjuration",
 };
 
 export function getBreweryProfileRoute(slug: string) {
