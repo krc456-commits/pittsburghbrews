943b2160acf7abd92561e394f6b8d0dea77d2e00
krc456-commits <krc.456@gmail.com>
2026-09-23 16:02:57 -0400

    Use new Allusion East End and 11th Hour assets

--- src/data/allBreweries.ts
+++ src/data/allBreweries.ts
@@ -47,6 +47,14 @@ const imageOverrides: Record<string, BreweryImage> = {
     "/brand/breweries/aslin/storefront.png",
     "Aslin Beer Company storefront in Pittsburgh"
   ),
+  "east-end": localImage(
+    "/brand/breweries/eastend/storefront.png",
+    "East End Brewing Company storefront in Pittsburgh"
+  ),
+  "eleventh-hour": localImage(
+    "/brand/breweries/eleventhhour/storefront.png",
+    "11th Hour Brewing Co. storefront in Lawrenceville"
+  ),
   "burghers-lawrenceville": localImage(
     "/brand/breweries/burghers/lawrenceville-storefront.png",
     "Burgh'ers Brewing Lawrenceville storefront"
@@ -80,6 +88,22 @@ const logoOverrides: Record<string, BreweryLogo> = {
   "dancing-gnome": {
     url: "/brand/breweries/dancinggnome/logo.png",
     alt: "Dancing Gnome logo"
+  },
+  "allusion-vandergrift": {
+    url: "/brand/breweries/allusion/logo.png",
+    alt: "Allusion Brewing Company logo"
+  },
+  "allusion-allison-park": {
+    url: "/brand/breweries/allusion/logo.png",
+    alt: "Allusion Brewing Company logo"
+  },
+  "east-end": {
+    url: "/brand/breweries/eastend/logo.png",
+    alt: "East End Brewing Company logo"
+  },
+  "eleventh-hour": {
+    url: "/brand/breweries/eleventhhour/logo.png",
+    alt: "11th Hour Brewing Co. logo"
   },
   "brew-gentlemen": {
     url: "/brand/breweries/brewgentlemen/logo.png",
